845b2971e9a2edcd35b6cb287a41effe5351f8f6
JikaelN <jikael.nt@outlook.com>
2024-11-29 18:55:00 +0100

    update
    update

--- run_replicate.sh
+++ run_replicate.sh
@@ -2,31 +2,25 @@
 
 # Set the path to the SLiM executable
 SLIM_EXEC="C:/msys64/mingw64/bin/slim.exe"
-SIM_SCRIPT="C:/Users/User/Desktop/Master project/Simulation/Neutral.slim"
+SIM_SCRIPT="C:/Users/SwissHardware/Desktop/Master-Project/Simulation/Neutral.slim"
 NUM_REPLICATES=100
 OUTPUT_DIR="./R_analysis/data/neutral_raw"
 SAMPLE_DIR="./R_analysis/data/processed"
 
 mkdir -p "$OUTPUT_DIR"
-mkdir -p "$SAMPLE_DIR"
+
 
 for (( i=1; i<=NUM_REPLICATES; i++ ))
 do
-    REPLICATE_DIR="$OUTPUT_DIR/replicate_$i"
-    mkdir -p "$REPLICATE_DIR"
-
     SEED=$(( RANDOM + i ))
     echo "Running replicate $i with seed $SEED..."
-    "$SLIM_EXEC" -s "$SEED" "$SIM_SCRIPT" > "$REPLICATE_DIR/log.txt"
+    "$SLIM_EXEC" -s "$SEED" "$SIM_SCRIPT" > "$OUTPUT_DIR/$i_log_$SEED.txt"
 
     if [[ $? -ne 0 ]]; then
         echo "Replicate $i failed."
     else
-        mv *.vcf "$REPLICATE_DIR/" 2>/dev/null
-		mv *.txt "$REPLICATE_DIR/" 2>/dev/null
-        python preprocessing.py "$REPLICATE_DIR" "$SAMPLE_DIR"
-        echo "Replicate $i preprocessing completed."
+        mv *.vcf "$OUTPUT_DIR/" 2>/dev/null
     fi
 done
 
-echo "All replicates completed. Results stored in $SAMPLE_DIR."
+echo "All replicates completed. Results stored in $OUTPUT_DIR."
